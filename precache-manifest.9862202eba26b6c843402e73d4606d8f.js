39dfb46bacc80afa290fcdab4aac47b440023b6a
Mariano Perdomo <mperdomo@elementum.com>
2019-04-12 16:58:45 -0300

    Updates

---
+++ precache-manifest.9862202eba26b6c843402e73d4606d8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3a642439be53fcfe582",
+    "url": "/create-nave-app/static/css/main.af653f89.chunk.css"
+  },
+  {
+    "revision": "d3a642439be53fcfe582",
+    "url": "/create-nave-app/static/js/main.79943ab7.chunk.js"
+  },
+  {
+    "revision": "545ff6645e630c231cd3",
+    "url": "/create-nave-app/static/js/runtime~main.298a19f9.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/create-nave-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "480d72037b455798eb1f8a63e01e4dcb",
+    "url": "/create-nave-app/static/media/nave.480d7203.png"
+  },
+  {
+    "revision": "7868634300dd4267168baa7d5fead3ee",
+    "url": "/create-nave-app/index.html"
+  }
+];
