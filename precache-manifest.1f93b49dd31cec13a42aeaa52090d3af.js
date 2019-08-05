1ce32a610884bffba175fba5682f357009ca06e4
Mariano <perdomo.mariano@gmail.com>
2019-08-05 02:22:12 -0300

    Updates

---
+++ precache-manifest.1f93b49dd31cec13a42aeaa52090d3af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ac970c9048adc2716a6d59c1301633",
+    "url": "/create-nave-app/index.html"
+  },
+  {
+    "revision": "b8569a1669aeb0fc1dcb",
+    "url": "/create-nave-app/static/css/main.7fc79a15.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/create-nave-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "b8569a1669aeb0fc1dcb",
+    "url": "/create-nave-app/static/js/main.577bd0eb.chunk.js"
+  },
+  {
+    "revision": "545ff6645e630c231cd3",
+    "url": "/create-nave-app/static/js/runtime~main.298a19f9.js"
+  },
+  {
+    "revision": "480d72037b455798eb1f8a63e01e4dcb",
+    "url": "/create-nave-app/static/media/nave.480d7203.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/create-nave-app/precache-manifest.c11ce142b606d03c2e99dee7f2b96adf.js"
+  "/create-nave-app/precache-manifest.1f93b49dd31cec13a42aeaa52090d3af.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/create-nave-app/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/create-nave-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
