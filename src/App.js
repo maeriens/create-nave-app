a73b2cf12e115977e23e9a39be41aaa3a02acff7
Mariano Perdomo <perdomo.mariano@gmail.com>
2020-01-24 12:57:00 -0300

    Now with more Gabi

--- src/App.js
+++ src/App.js
@@ -1,32 +1,49 @@
 import React, { Component } from 'react';
-import logo from './nave.png';
+import Konami from 'react-konami-code';
+import nave from './nave.png';
+import gabi from './gabi.png';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    logo: null
+  }
+
+  egg = () => {
+    this.setState({ logo: gabi })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Mandale cumbia y <a
-              className="repo"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/maeriens/create-nave-app">
-              PR al repo
+      <>
+        <Konami
+          action={this.egg}
+          code={[71, 65, 66, 73]}
+        />
+        <div className="App">
+          <header className="App-header">
+            <img src={this.state.logo || nave} className="App-logo" alt="logo" />
+            <p>
+              Mandale cumbia y <a
+                className="repo"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/maeriens/create-nave-app">
+                PR al repo
             </a> y sale con fritas
           </p>
-          <a
-            className="App-link"
-            href="https://www.instagram.com/federiconaveira/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Peer into the Nave
+            <a
+              className="App-link"
+              href="https://www.instagram.com/federiconaveira/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Peer into the Nave
           </a>
-        </header>
-      </div>
+          </header>
+        </div>
+      </>
     );
   }
 }
