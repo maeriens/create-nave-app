6268358c30f84e000436c74ee4b54c7e4097a026
Mariano Perdomo <perdomo.mariano@gmail.com>
2020-07-08 15:43:00 -0300

    Nave deja la nave

--- src/App.js
+++ src/App.js
@@ -6,13 +6,9 @@ import './App.css';
 
 class App extends Component {
 
-  state = {
-    logo: null
-  }
+  state = { logo: null };
 
-  egg = () => {
-    this.setState({ logo: gabi })
-  }
+  egg = () => this.setState({ logo: gabi });
 
   render() {
     return (
@@ -24,6 +20,9 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={this.state.logo || nave} className="App-logo" alt="logo" />
+            <p>In Nave Memorian</p>
+            <p>2016 - 2020</p>
+            <p>No se murió pero en Suecia debe ser de noche y estará durmiendo</p>
             <p>
               Mandale cumbia y <a
                 className="repo"
